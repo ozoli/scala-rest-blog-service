0dfd34f72bec4f7a2f7b060b10a0cf62573b7393
Oliver Carr <oliver.carr@gmail.com>
2015-09-14 21:33:06 +0200

    parse the publication date and add comments

--- src/main/scala/io/ozoli/blog/RssReader.scala
+++ src/main/scala/io/ozoli/blog/RssReader.scala
@@ -2,31 +2,39 @@ package io.ozoli.blog
 
 import java.net.URL
 
-import org.joda.time.DateTime
+import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
 
 import scala.xml.{Elem, Node, XML}
 
+/**
+ * A RSS Reader from XML or URL create Blog Entry's
+ */
 object RssReader {
 
-  def getBlogEntries(xml: Elem) : Seq[BlogEntry] = {
-    val channel = xml \\ "channel"
-    extractBlogEntries(channel.head)
-  }
+  lazy val pubDateFormat : DateTimeFormatter = DateTimeFormat.forPattern("EEE, dd MMM yyyy HH:mm:ss Z")
 
-  def extractRss(urlStr : String) : Seq[BlogEntry] = {
-    // Given a URL, returns a Seq of RSS data in the form:
-    // ("channel", ["article 1", "article 2"])
-    val url = new URL(urlStr)
-    val conn = url.openConnection
-    val xml = XML.load(conn.getInputStream)
+  /**
+   * For the given XML retreive the BlogEntry's found
+   * @param xml the XML element to parse
+   * @return the Seq of BlogEntry's found
+   */
+  def getBlogEntries(xml: Elem) : Seq[BlogEntry] = extractBlogEntries((xml \\ "channel").head)
 
-    extractBlogEntries(xml)
-  }
+  /**
+   * For the given URL extract the BlogEntry's contained within
+   * @param url the URL to use
+   * @return the Seq of BlogEntry's found
+   */
+  def extractRss(url : URL) : Seq[BlogEntry] = extractBlogEntries(XML.load(url.openConnection.getInputStream))
 
+  /**
+   * Given a channel node from an RSS feed, returns all of the BlogEntries
+   * @param channel the XML Node to read all the blog entries from
+   * @return a Seq of BlogEntry
+   */
   private def extractBlogEntries(channel : Node) : Seq[BlogEntry] = {
-    // Given a channel node from an RSS feed, returns all of the BlogEntries
     for (item <- channel \\ "item") yield {
-      BlogEntry(1, new DateTime(),
+      BlogEntry(1, pubDateFormat.parseDateTime((item \ "pubDate").text).toLocalDateTime,
         (item \ "title").text,
         (item \ "description").text,
         (channel \\ "category").map(_.text).mkString(" "))
