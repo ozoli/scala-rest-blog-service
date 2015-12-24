328ec13650391ebb361bc776842006697fab5216
Oliver Carr <oliver.carr@gmail.com>
2015-12-24 08:38:40 +0100

    add logging and fallback local RSS feed XML file

--- src/main/scala/io/ozoli/blog/util/RssReader.scala
+++ src/main/scala/io/ozoli/blog/util/RssReader.scala
@@ -2,16 +2,17 @@ package io.ozoli.blog.util
 
 import java.net.URL
 
+import grizzled.slf4j.Logger
 import io.ozoli.blog.domain.BlogEntry
 import org.joda.time.format.{DateTimeFormatterBuilder, DateTimeFormat, DateTimeFormatter}
 
 import scala.xml.{Elem, Node, XML}
 
-/**
- * A RSS Reader from XML or URL create Blog Entry's
- */
+ // A RSS Reader from XML or URL create Blog Entry's
 object RssReader {
+  val logger = Logger[this.type]
 
+  // support different formats of the publish date in the Atom RSS Feed.
   lazy val pubDateFormat : DateTimeFormatter = new DateTimeFormatterBuilder().append( null, Array(
     DateTimeFormat.forPattern( "EEE, dd MMM yyyy HH:mm:ss Z" ).getParser,
     DateTimeFormat.forPattern( "EEE, dd MMM yyyy HH:mm:ss z" ).getParser )).toFormatter
@@ -28,7 +29,17 @@ object RssReader {
    * @param url the URL to use
    * @return the Seq of BlogEntry's found
    */
-  def extractRss(url : URL) : Seq[BlogEntry] = extractBlogEntries(XML.load(url.openConnection.getInputStream))
+  def extractRss(url : URL) : Seq[BlogEntry] = {
+      try {
+        extractBlogEntries(XML.load(url.openConnection.getInputStream))
+      }
+      catch {
+        case e : Exception =>
+          logger.error(s"Error Finding Blogs for URL %s %s %s".format(url, e.getMessage, e))
+          logger.warn(s"Fallback Blog List in use!")
+          getBlogEntries(XML.load(getClass.getResource("/blog-entries.xml").openStream()))
+      }
+  }
 
   /**
    * Given a channel node from an RSS feed, returns all of the BlogEntries
