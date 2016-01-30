7a8c2af64cd41bb323edca4d247f5d0506fb3749
Oliver Carr <oliver.carr@gmail.com>
2016-01-30 10:47:39 +0200

    update set env script

--- setEnv.sh
+++ setEnv.sh
@@ -3,13 +3,12 @@
 # Set environment variables for starting the Blog Service
 
 export BLOG_APP_HOSTNAME=localhost
-export BLOG_APP_PORT=8180
+export BLOG_APP_PORT=9999
 
-export BLOG_DB_USER=scala
-export BLOG_DB_PASSWORD=password
-export BLOG_DB_NAME=yourblogdb
-export BLOG_DB_HOST=localhost
-export BLOG_DB_PORT=3306
+export BLOG_DB_URI=mongodb://localhost:27017
+export BLOG_DB_NAME=blogdb
+export BLOG_DB_COLLECTIONNAME=blogs
 
 export BLOG_RSS_URI=http://your.feed.host/YourBlog?fmt=xml
 
+export COVERALLS_REPO_TOKEN=your-coveralls-token
